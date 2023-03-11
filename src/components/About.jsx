638f7009a56a3e116400fd85765c8ac0380f22ea
MedEzzedine <mohamedezzedine2000@gmail.com>
2023-03-11 14:33:04 +0100

    Started working on about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { services } from "../constants";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+        consectetur condimentum dui tincidunt sagittis. Quisque interdum
+        elementum nunc, non dignissim ex. Vestibulum iaculis euismod neque sit
+        amet blandit. Nunc bibendum in massa id cursus. Aliquam erat volutpat.
+        In imperdiet, justo et egestas efficitur, urna eros sodales ipsum,
+        congue pharetra justo massa vel erat.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
