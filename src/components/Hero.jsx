70ff543cb268a53f198b53f6ddb0d760c28b1764
MedEzzedine <mohamedezzedine2000@gmail.com>
2023-03-11 13:38:15 +0100

    Added scrolling indicator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { styles } from "../styles";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -16,9 +17,28 @@ const Hero = () => {
             Hello, I'm &nbsp;<span className="text-[#915eff]">Mohamed</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm an AWS Cloud/DevOps Engineer
+            I'm an AWS <br className="sm:block hidden" />
+            Cloud/DevOps Engineer
           </p>
         </div>
+      </div>
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
