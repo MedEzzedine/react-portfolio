b72c6efcf259aeae45f2479b3be51651c0df53a5
MedEzzedine <mohamedezzedine2000@gmail.com>
2023-03-20 11:19:44 +0100

    Added icons in technology section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { SectionWrapper } from "./higherOrderComponent";
+import { technologies } from "../constants";
+
+// Tech folder location
+const techFolder = "../assets/tech";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-wrap justify-center gap-10">
+      {technologies.map((tech) => (
+        <div className="w-28 h-28" key={tech.name}>
+          <img src={tech.icon} alt={tech.name} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "tech");
