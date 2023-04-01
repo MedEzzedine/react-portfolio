e003c6a4261c477c072b7b19b23a339f376b3c99
MedEzzedine <mohamedezzedine2000@gmail.com>
2023-04-01 14:10:30 +0100

    Added projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,44 @@ const ProjectCard = ({
   github_link,
 }) => (
   <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-    test
+    <Tilt
+      options={{ max: 45, scale: 1, speed: 450 }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+    >
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div
+            onClick={() => window.open(github_link, "_blank")}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={github}
+              alt="Github"
+              className="w-1/2 h-1/2 object-contain"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <div className="rounded-full bg-primary text-center pl-5 pr-5 text-gray-100">
+            {tag.name}
+          </div>
+        ))}
+      </div>
+    </Tilt>
   </motion.div>
 );
 
