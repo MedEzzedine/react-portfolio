29842b56bb97e5e83a87ad9272e26a350103c537
MedEzzedine <mohamedezzedine2000@gmail.com>
2023-04-09 14:27:28 +0100

    Fixed send button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,7 +65,9 @@ const Contact = () => {
           <button
             type="submit"
             className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
-          ></button>
+          >
+            Send
+          </button>
         </form>
       </motion.div>
 
